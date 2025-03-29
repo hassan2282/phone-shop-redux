792ccfd5c0774863945da86938206f332a28de1a
hassan2282 <taghavey.hassan@gmail.com>
2025-03-29 16:25:48 +0330

    edit register form css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,36 +8,41 @@ function Navbar() {
   const initialState = useSelector((store)=>store);
 
   return (
-    <div className='flex flex-row sticky top-0 justify-between items-center text-white h-20 z-20 bg-zinc-800 opacity-90  w-full px-4' dir='rtl'>
-        <ul className='flex flex-row gap-6 hover:*:scale-110 mr-10 hover:*:text-yellow-400 *:cursor-pointer hover:*:duration-150'>
-            <li>خانه</li>
-            <li>محصولات</li>
-            <li>درباره ما</li>
-            <li>تماس با ما</li>
-            <Link to={'/register'}>
-              <li>ورود / ثبت نام</li>
-            </Link>
-        </ul>
+    <>
+      <div className='flex flex-row sticky top-0 justify-between items-center text-white h-20 z-20 bg-zinc-800 opacity-90  w-full px-4' dir='rtl'>
+          <ul className='flex flex-row gap-6 hover:*:scale-110 mr-10 hover:*:text-yellow-400 *:cursor-pointer hover:*:duration-150'>
+              <li>خانه</li>
+              <li>محصولات</li>
+              <li>درباره ما</li>
+              <li>تماس با ما</li>
+              <Link to={'/register'}>
+                <li>ورود / ثبت نام</li>
+              </Link>
+          </ul>
 
-        <div className='flex flex-row gap-6 h-full items-center hover:*:scale-95 hover:*:text-yellow-400 *:cursor-pointer *:duration-300'>
-          <FaMagnifyingGlass  size={28}/>
-          <FaAward size={28}/>
-          <div className='relative flex flex-row justify-center items-center'>
-            <FaCartShopping  size={28}/>
-            {
-              initialState.addData &&
-              <span className='absolute text-[12px] w-[40px] -top-3 -left-1 text-center flex items-center justify-center bg-yellow-400 text-black rounded-full'>
-                {
-                  initialState.addData.length
-                }
-              </span>
-            }
+          <div className='flex flex-row gap-6 h-full items-center hover:*:scale-95 hover:*:text-yellow-400 *:cursor-pointer *:duration-300'>
+            <FaMagnifyingGlass  size={28}/>
+            <FaAward size={28}/>
+            <div className='relative flex flex-row justify-center items-center'>
+              <FaCartShopping  size={28}/>
+              {
+                initialState.addData &&
+                <span className='absolute text-[12px] w-[40px] -top-3 -left-1 text-center flex items-center justify-center bg-yellow-400 text-black rounded-full'>
+                  {
+                    initialState.addData.length
+                  }
+                </span>
+              }
+            </div>
+            <Link to={'/'}>
+                <img src={require('../pics/1.png')} alt='logo' className='rotate-90 w-14 rounded-md hover:rotate-45 hover:scale-75 duration-300'/>
+            </Link>
           </div>
-          <Link to={'/'}>
-              <img src={require('../pics/1.png')} alt='logo' className='rotate-90 w-14 rounded-md hover:rotate-45 hover:scale-75 duration-300'/>
-          </Link>
-        </div>
-    </div>
+      </div>
+          {/* <div className='absolute z-30 bg-stone-700 w-screen h-screen'>
+
+          </div> */}
+    </>
   )
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function Register() {
   return (
-    <div className='flex flex-col fixed top-0 z-40 bg-slate-900 opacity-[90%] items-center justify-center w-screen h-screen '>
+    <div className='flex flex-col fixed top-0 z-40 bg-slate-900 bg-opacity-90 items-center justify-center w-screen h-screen '>
             <form className='flex flex-col w-[60%] relative bg-zinc-50 border gap-5 rounded-[100px] h-[70%] p-2 items-center before:-z-10 before:content-[""] before:inset-0 before:h-full before:w-full before:scale-105 before:rounded-full  before:border-2 before:absolute'>
                 <Link to={'/'}>
                   <FiXOctagon  size={24} className='absolute left-10 top-10 m-3 text-red-600' />
@@ -13,7 +13,7 @@ function Register() {
                 <div className='flex flex-col w-full h-full items-center justify-center *:bg-slate-900 *:p-3 *:border *:rounded-md gap-3 *:w-[70%]' dir='rtl'>
                     <input type='email' placeholder='ایمیل خود را وارد کنید' className='text-white'/>
                     <input type='password' placeholder='پسورد خود را وارد کنید' className='text-white' />
-                    <button className='text-white hover:bg-yellow-600 duration-300'>ثبت نام</button>
+                    <button className='text-white hover:bg-slate-950 duration-300'>ثبت نام</button>
                 </div>
             </form>
     </div>
